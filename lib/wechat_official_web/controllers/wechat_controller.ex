c0b00f518f4eaecf5b55b5bbc57f508b743a01bb
LU JI <jjpro@LUs-MacBook-Pro-3.local>
2018-05-10 03:24:30 -0400

    added wechat response template text.xml.eex

--- lib/wechat_official_web/controllers/wechat_controller.ex
+++ lib/wechat_official_web/controllers/wechat_controller.ex
@@ -9,8 +9,8 @@ defmodule WechatOfficialWeb.WechatController do
   end
 
   def create(conn, _params) do
-    _params  |> IO.inspect(label: ">>>>> params")
-    msg = conn.body_params |> IO.inspect(label: ">>>>> conn.body_params")
+    # _params  |> IO.inspect(label: ">>>>> params")
+    msg = conn.body_params |> IO.inspect(label: ">>>>> msg = conn.body_params")
     reply = build_text_reply(msg)
     render conn, "text.xml", reply: reply
   end
@@ -18,5 +18,10 @@ defmodule WechatOfficialWeb.WechatController do
   defp build_text_reply(%{"ToUserName" => to, "FromUserName" => from, "Content" => content}) do
     # FromUserName is user's OpenID to our official account
     %{from: to, to: from, content: "Hi thanks for your \n content " <> content}
+  end
+
+  defp build_text_reply(%{"ToUserName" => to, "FromUserName" => from, "PicUrl" => url}) do
+    # FromUserName is user's OpenID to our official account
+    %{from: to, to: from, content: "Hi thanks for your \n content " <> url}
   end
 end
